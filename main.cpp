65a75b345d70f149266cf218e833a98c43210256
ThePhD <phdofthehouse@gmail.com>
2013-12-09 00:25:47 -0500

    Well, we fixed Raster Font loading and all (but we still have to test it against NymphBatch). Will take a break from this for a bit and try to work out OpenGL and raytracing stuff.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,22 @@
 #include <Furrovine++/WindowDriver.h>
 #include <Furrovine++/ServiceTracker.h>
 #include <Furrovine++/IWindowService.h>
+#include <Furrovine++/Text/RasterFont.h>
 #include <Furrovine++/Graphics/Window.h>
 #include <Furrovine++/Graphics/GraphicsDevice.h>
+#include <Furrovine++/Pipeline/RasterFontDataLoader.h>
+#include <Furrovine++/Pipeline/RasterFontLoader.h>
+#include <Furrovine++/Pipeline/TextureLoader.h>
+#include <Furrovine++/Pipeline/ImageLoader.h>
+#include <Furrovine++/Pipeline/JPGSaver.h>
+#include <Furrovine++/Pipeline/PNGSaver.h>
+#include <Furrovine++/IO/FileStream.h>
 
 using namespace Furrovine;
+using namespace Furrovine::IO;
 using namespace Furrovine::Graphics;
+using namespace Furrovine::Pipeline;
+using namespace Furrovine::Text;
 
 class Snaku : public FurrovineGame {
 private:
@@ -18,13 +29,17 @@ private:
 	GraphicsDevice graphics;
 	IWindowServiceTracker windowservice;
 	IGraphicsDeviceServiceTracker graphicsservice;
-
+	
 public:
-	Snaku( ) : FurrovineGame( ), 
-	windowdriver( ), 
-	window( windowdriver, WindowDescription{ ">=== Snaku ===<", WindowStyles::Bordered } ),
-	windowservice( Services, window ), graphicsservice( Services, graphics ) {
+	Snaku( ) : FurrovineGame( ),
+		windowdriver( ),
+		window( windowdriver, WindowDescription( "~ Snaku ~", WindowStyles::BorderlessTransparent ) ),
+		windowservice( Services, window ), graphicsservice( Services, graphics ) {
 		window.Show( );
+	}
+
+	void Initialize( ) {
+	
 	}
 
 	void Loop( ) {
@@ -41,6 +56,7 @@ public:
 
 	void Render( ) {
 		graphics.Clear( Color( 20, 20, 50, 50 ) );
+
 	}
 
 };
