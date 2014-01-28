3a308a0b90c22fefc1dadd53554e5a180e564a29
ThePhD <phdofthehouse@gmail.com>
2014-01-28 18:55:24 -0500

    Alright, image shader is all good! That took a lot of extra effort for a simple problem...

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@
 #include <Furrovine++/IO/FileStream.h>
 #include <Furrovine++/Format.h>
 #include <Furrovine++/Text/TextResources.h>
+#include <Furrovine++/Graphics/VertexPosition.h>
 #include <Furrovine++/Graphics/VertexImageQuad.h>
 #include <Furrovine++/Pipeline/HLSLEffectLoader.h>
 
@@ -95,76 +96,6 @@ protected:
 
 	}
 
-	void tryit( ) {
-		Viewport viewport = graphics.GetViewport( );
-		Matrix projection = Matrix::CreateOrthographicProjectionOffCenter(
-			viewport.x, viewport.x + viewport.width,
-			viewport.y + viewport.height, viewport.y,
-			-1.0f, 1.0f );
-
-		uint16 indices[ 6 ] = {
-			0, 1, 2,
-			1, 3, 2
-		};
-		float right = 500;
-		float bottom = 500;
-		float texW = static_cast<float>( texture->Width( ) );
-		float texH = static_cast<float>( texture->Height( ) );
-		float lefttexel = 0 / texW;
-		float righttexel = ( 0 + 500 ) / texW;
-		float toptexel = 0 / texH;
-		float bottomtexel = ( 0 + 500 ) / texH;
-		Region bounds( 0, 0, 500, 500 );
-		Matrix world = Matrix::Identity;
-		world.m11 = bounds.width / texW;
-		world.m22 = bounds.height / texH;
-
-		world.m41 = bounds.x;
-		world.m42 = bounds.y;
-
-		VertexImageQuad quad[ 4 ] = {
-			{ },
-			{ },
-			{ },
-			{ },
-		};
-		VertexImageQuad quadvertex[ 6 ] = {
-			{ },
-			{ },
-			{ },
-			{ },
-		};
-
-		Color tint = Colors::White;
-
-		quad[ 0 ].color = tint;
-		quad[ 0 ].position.x = ( world.m11 * 0 ) + ( world.m21 * 0 ) + ( world.m31 * 0 ) + ( world.m41 );
-		quad[ 0 ].position.y = ( world.m12 * 0 ) + ( world.m22 * 0 ) + ( world.m32 * 0 ) + ( world.m42 );
-		quad[ 0 ].position.zw = Vector2::Zero;
-
-		quad[ 1 ].color = tint;
-		quad[ 1 ].position.x = ( world.m11 * right ) + ( world.m21 * 0 ) + ( world.m31 * 0 ) + ( world.m41 );
-		quad[ 1 ].position.y = ( world.m12 * right ) + ( world.m22 * 0 ) + ( world.m32 * 0 ) + ( world.m42 );
-		quad[ 1 ].position.zw = Vector2::Zero;
-
-		quad[ 2 ].color = tint;
-		quad[ 2 ].position.x = ( world.m11 * 0 ) + ( world.m21 * bottom ) + ( world.m31 * 0 ) + ( world.m41 );
-		quad[ 2 ].position.y = ( world.m12 * 0 ) + ( world.m22 * bottom ) + ( world.m32 * 0 ) + ( world.m42 );
-		quad[ 2 ].position.zw = Vector2::Zero;
-
-		quad[ 3 ].color = tint;
-		quad[ 3 ].position.x = ( world.m11 * right ) + ( world.m21 * bottom ) + ( world.m31 * 0 ) + ( world.m41 );
-		quad[ 3 ].position.y = ( world.m12 * right ) + ( world.m22 * bottom ) + ( world.m32 * 0 ) + ( world.m42 );
-		quad[ 3 ].position.zw = Vector2::Zero;
-		( *effect )[ 0 ][ 0 ].Parameters( )[ "ViewProjection" ].SetValue( projection );
-		( *effect )[ 0 ][ 0 ].Apply( );
-		//RenderUserIndexedPrimitives( PrimitiveTopology::TriangleList, quad, 0, 4, indices, 0, 2 );
-		for ( ulword i = 0; i < 6; ++i ) {
-			quadvertex[ i ] = quad[ indices[ i ] ];
-		}
-		graphics.RenderUserPrimitives( PrimitiveTopology::TriangleList, quadvertex, 0, 2 );
-	}
-
 	void Render( ) {
 		graphics.Clear( Color( 96, 96, 128, 128 ) );
 		//graphics2d.RenderText( *font, L" ♡ Woof ♡" );
@@ -173,8 +104,7 @@ protected:
 		//qbatch->Render( *texture, Colors::White, Region( 0, 200, 200, 200 ) );
 		//qbatch->Render( *texture, Colors::White, Region( 200, 200, 200, 200 ) );
 		//qbatch->End( );
-		tryit( );
-		//graphics.RenderImage( *texture );
+		graphics.RenderImage( *texture );
 	}
 
 };
